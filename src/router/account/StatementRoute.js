f81e12ed240ff00bf5e9abf3b816fb11401680b9
choissohyun <thgus429@naver.com>
2023-05-15 19:39:40 +0900

    라우터,아이콘 수정

--- src/router/account/StatementRoute.js
+++ src/router/account/StatementRoute.js
@@ -1,4 +1,46 @@
 const StatementRoute = [
+
+  // 계정별 원장
+  {
+    path: '/acc/base/accountLedger',
+    name: 'accountLedger',
+    // eslint-disable-next-line import/no-unresolved
+    component: () => import('@/views/account/base/AccountLedgerPage.vue'),
+    meta: {
+      pageTitle: '계정별원장',
+      breadcrumb: [
+        {
+          text: '장부관리',
+          //active: true,
+        },
+        {
+          text: '계정별원장',
+          active: true,
+        },
+      ],
+    },
+  },
+
+  // 총계정원장
+  {
+    path: '/acc/base/generalAccountLedger',
+    name: 'generalAccountLedger',
+    // eslint-disable-next-line import/no-unresolved
+    component: () => import('@/views/account/base/GeneralAccountLedgerPage.vue'),
+    meta: {
+      pageTitle: '총계정원장',
+      breadcrumb: [
+        {
+          text: '장부관리',
+        },
+        {
+          text: '총계정원장',
+          active: true,
+        },
+      ],
+    },
+  },
+
   /**
      * 일계표
      */
@@ -7,6 +49,18 @@ const StatementRoute = [
     name: 'detailTrialBalance',
     // eslint-disable-next-line import/no-unresolved
     component: () => import('@/views/account/statement/DetailTrialBalancePage.vue'),
+    meta: {
+      pageTitle: '일(월)계표',
+      breadcrumb: [
+        {
+          text: '장부관리',
+        },
+        {
+          text: '일(월)계표',
+          active: true,
+        },
+      ],
+    },
   },
   /**
      * 현금 출납장
@@ -16,6 +70,18 @@ const StatementRoute = [
     name: 'cashJournal',
     // eslint-disable-next-line import/no-unresolved
     component: () => import('@/views/account/statement/CashJournalPage.vue'),
+    meta: {
+      pageTitle: '현금출납장',
+      breadcrumb: [
+        {
+          text: '장부관리',
+        },
+        {
+          text: '현금출납장',
+          active: true,
+        },
+      ],
+    },
   },
   /**
      * 합계잔액시산표.
@@ -25,6 +91,18 @@ const StatementRoute = [
     name: 'totalTrialBalance',
     // eslint-disable-next-line import/no-unresolved
     component: () => import('@/views/account/statement/TotalTrialBalancePage.vue'),
+    meta: {
+      pageTitle: '합계잔액시산표',
+      breadcrumb: [
+        {
+          text: '결산/재무표관리',
+        },
+        {
+          text: '합계잔액시산표',
+          active: true,
+        },
+      ],
+    },
   },
   /**
      * 손익계산서
@@ -34,6 +112,18 @@ const StatementRoute = [
     name: 'incomeStatement',
     // eslint-disable-next-line import/no-unresolved
     component: () => import('@/views/account/statement/IncomeStatementPage.vue'),
+    meta: {
+      pageTitle: '손익계산서',
+      breadcrumb: [
+        {
+          text: '결산/재무표관리',
+        },
+        {
+          text: '손익계산서',
+          active: true,
+        },
+      ],
+    },
   },
   /**
      * 재무상태표
@@ -43,6 +133,18 @@ const StatementRoute = [
     name: 'financialPosition',
     // eslint-disable-next-line import/no-unresolved
     component: () => import('@/views/account/statement/FinancialPositionPage.vue'),
+    meta: {
+      pageTitle: '재무상태표',
+      breadcrumb: [
+        {
+          text: '결산/재무표관리',
+        },
+        {
+          text: '재무상태표',
+          active: true,
+        },
+      ],
+    },
   },
   /**
      * 월별손익계산서
@@ -51,6 +153,18 @@ const StatementRoute = [
     path: '/acc/account/statement/monthIncomeStatement',
     name: 'monthIncomeStatement',
     component: () => import('@/views/account/statement/MonthIncomeStatementPage.vue'),
+    meta: {
+      pageTitle: '월별손익계산서',
+      breadcrumb: [
+        {
+          text: '결산/재무표관리',
+        },
+        {
+          text: '월별손익계산서',
+          active: true,
+        },
+      ],
+    },
   },
   /**
      * 현금흐름표
@@ -59,6 +173,18 @@ const StatementRoute = [
     path: '/acc/account/statement/cashFlowStatement',
     name: 'cashFlowStatement',
     component: () => import('@/views/account/statement/CashFlowStatementPage.vue'),
+    meta: {
+      pageTitle: '현금흐름표',
+      breadcrumb: [
+        {
+          text: '결산/재무표관리',
+        },
+        {
+          text: '현금흐름표',
+          active: true,
+        },
+      ],
+    },
   },
   /**
      * 원가명세서
@@ -67,6 +193,18 @@ const StatementRoute = [
     path: '/acc/account/statement/costStatement',
     name: 'costStatement',
     component: () => import('@/views/account/statement/CostStatementPage.vue'),
+    meta: {
+      pageTitle: '원가명세서',
+      breadcrumb: [
+        {
+          text: '결산/재무표관리',
+        },
+        {
+          text: '원가명세서',
+          active: true,
+        },
+      ],
+    },
   },
 
 ]
